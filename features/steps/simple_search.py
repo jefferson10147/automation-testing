b96325e9e45951a89107e574361beac0553505cd
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-19 07:24:16 -0400

    Create the structure for the simple test case

---
+++ features/steps/simple_search.py
@@ -0,0 +1,21 @@
+from behave import given, when, then
+
+
+@given('Open python page')
+def open_main_page(context):
+    pass
+
+
+@when('Verify the correct page is openned')
+def verify_python_page(context):
+    pass
+
+
+@when('Search for Pycon in the search bar')
+def search_in_search_bar(context):
+    pass
+
+
+@then('Verify there are not results')
+def verify_no_results(context):
+    pass
