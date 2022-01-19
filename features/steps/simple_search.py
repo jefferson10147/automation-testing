2f964857ff7b93dc2f121f585cdb166482b03cb8
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-19 07:38:32 -0400

    Implementation of the first test case for a simple search

--- features/steps/simple_search.py
+++ features/steps/simple_search.py
@@ -1,21 +1,29 @@
+from time import sleep
+
 from behave import given, when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 @given('Open python page')
 def open_main_page(context):
-    pass
+    context.driver.get('http://www.python.org')
 
 
 @when('Verify the correct page is openned')
 def verify_python_page(context):
-    pass
+    assert 'Python' in context.driver.title
 
 
 @when('Search for Pycon in the search bar')
 def search_in_search_bar(context):
-    pass
+    elem = context.driver.find_element(By.ID, 'id-search-field')
+    elem.clear()
+    elem.send_keys('pycon')
+    elem.send_keys(Keys.RETURN)
 
 
 @then('Verify there are not results')
 def verify_no_results(context):
-    pass
+    assert 'No results found.' not in context.driver.page_source
+    # sleep(5)
