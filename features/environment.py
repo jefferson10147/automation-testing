38b4658d2fc4ec7e3fc52fc40cd6ebd2ab682d33
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-19 07:13:29 -0400

    Implement the first version of feature file

---
+++ features/environment.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def browser_init(context):
+    """
+    :param context: Behave context
+    """
+    context.driver = webdriver.Chrome('./chromedriver.exe')
+    context.browser.maximize_window()
+    context.browser.implicitly_wait(5)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+
+
+def after_scenario(context, feature):
+    context.browser.delete_all_cookies()
+    context.browser.quit()
+    
