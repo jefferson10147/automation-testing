3baa56b8614e4a07f7bc0623616bcde11ef3633f
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-26 23:18:27 -0400

    Define the step when: to locate amazon logo

--- features/steps/signin_page.py
+++ features/steps/signin_page.py
@@ -5,3 +5,7 @@ from behave import given, when, then
 def open_page(context):
     context.app.sign_in_page.open_page()
     
+
+@when('Verify the correct page is openned')
+def verify_the_page(context):
+    pass
