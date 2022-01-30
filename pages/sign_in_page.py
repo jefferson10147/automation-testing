31fc0e76ab1e3f4621fa0e3433f680ec3e10d622
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-30 14:54:03 -0400

    Amazon Logo: define the locator and create the class function to locate it

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -2,11 +2,19 @@ from turtle import st
 from decouple import config
 from pages.base_page import Page
 
+from selenium.webdriver.common.by import By
+
 
 SING_IN_PAGE = config('SIGNIN_PAGE')
 
 
 class SignIn(Page):
 
+    AMAZON_LOGO = (By.CSS_SELECTOR, 'i.a-icon.a-icon-logo')
+
     def open_page(self, end_url=SING_IN_PAGE) -> None:
         super().open_page(end_url)
+
+    def locate_amazon_logo(self):
+        self.find_element(*self.AMAZON_LOGO)
+        
