02426c4f359db9a242c3b11aab7f5aa664030225
Jefferson <jeffersonmontilla.98@gmail.com>
2022-01-30 15:12:33 -0400

    Email field: create the locator and define the class function

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -11,10 +11,13 @@ SING_IN_PAGE = config('SIGNIN_PAGE')
 class SignIn(Page):
 
     AMAZON_LOGO = (By.CSS_SELECTOR, 'i.a-icon.a-icon-logo')
+    EMAIL_FIELD = (By.ID, 'ap_email')
 
     def open_page(self, end_url=SING_IN_PAGE) -> None:
         super().open_page(end_url)
 
     def locate_amazon_logo(self):
         self.find_element(*self.AMAZON_LOGO)
-        
+
+    def locate_email_field(self):
+        self.find_element(*self.EMAIL_FIELD)
