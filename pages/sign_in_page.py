72f8badce18fca80e02a1ccf6f6608cbb10927ce
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-01 12:06:32 -0400

    Conditions of use link: locator and function to locate it

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -13,6 +13,7 @@ class SignIn(Page):
     AMAZON_LOGO = (By.CSS_SELECTOR, 'i.a-icon.a-icon-logo')
     EMAIL_FIELD = (By.ID, 'ap_email')
     CONTINUE_BTN = (By.ID, 'continue')
+    CONDITIONS_OF_USE = (By.CSS_SELECTOR, 'a[href *= "ref=ap_signin_notification_condition_of_use?"]')
 
     def open_page(self, end_url=SING_IN_PAGE) -> None:
         super().open_page(end_url)
@@ -26,3 +27,7 @@ class SignIn(Page):
     def locate_continue_btn(self):
         expected_text = 'Continue'
         self.verify_text(expected_text, *self.CONTINUE_BTN)
+
+    def locate_conditions_link(self):
+        expected_text = 'Conditions of Use'
+        self.verify_text(expected_text, *self.CONDITIONS_OF_USE)
