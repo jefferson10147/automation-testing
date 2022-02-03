bd75871b9b86de0fcc08ada15eda7b0ac49e168f
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-03 08:30:02 -0400

    Help page test case, user can search for cancel orders

--- features/steps/help_page_steps.py
+++ features/steps/help_page_steps.py
@@ -3,14 +3,14 @@ from behave import given, when, then
 
 @given('Open Amazon help page')
 def open_help_page(context):
-    pass
+    context.app.help_page.open_page()
 
 
 @when('Search for cancel order')
 def search_in_searchbar(context):
-    pass
+    context.app.help_page.search_for_cancel_orders()
 
 
 @then('Verify Cancel Items or Orders text is present')
 def verify_text_is_present(context):
-    pass
+    context.app.help_page.verify_cancel_order_text()
--- pages/help_page.py
+++ pages/help_page.py
@@ -1,5 +1,6 @@
 from decouple import config
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from pages.base_page import Page
 
@@ -9,5 +10,18 @@ HELP_PAGE = config('HELP_PAGE')
 
 class HelpPage(Page):
     
+    SEARCH_BAR = (By.ID, 'helpsearch')
+    CANCEL_ITEMS_TEXT = (By.CSS_SELECTOR, 'div.help-content h1')
+
     def open_page(self, end_url=HELP_PAGE) -> None:
         super().open_page(end_url)
+
+    def search_for_cancel_orders(self):
+        text = 'Cancel order'
+        self.input_text(text, *self.SEARCH_BAR)
+        self.input_keys(Keys.RETURN, *self.SEARCH_BAR)
+
+    def verify_cancel_order_text(self):
+        text = 'Cancel Items or Orders'
+        self.wait_for_element_appear(*self.CANCEL_ITEMS_TEXT)
+        self.verify_text(text, *self.CANCEL_ITEMS_TEXT)
