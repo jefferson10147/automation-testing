abddab9bfe68e268db75d4b7dafea38a76a33b23
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-03 08:03:46 -0400

    Connect help page model with application

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 from pages.sign_in_page import SignIn
+from pages.help_page import HelpPage
 
 
 class Application:
@@ -6,4 +7,4 @@ class Application:
     def __init__(self, driver) -> None:
         self.driver = driver
         self.sign_in_page = SignIn(self.driver)
-        
+        self.help_page = HelpPage(self.driver)
