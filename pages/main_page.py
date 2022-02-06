1440e70c33398c9cbcc5702dc76b67cafe17080c
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-06 17:41:42 -0400

    Cart empty testcase: locators and logic in page model object

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,4 +5,16 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(Page):
-    pass
+
+    CART = (By.ID, 'nav-cart-count-container')
+    EMPTY_CART_TEXT = (By.CSS_SELECTOR, '.a-row.sc-your-amazon-cart-is-empty h2')
+
+    def open_amazon_main_page(self):
+        self.open_page()
+
+    def click_in_cart_icon(self):
+        self.click(*self.CART)
+
+    def verify_empty_cart_text(self):
+        expected_text = 'Your Amazon Cart is empty'
+        self.verify_text(expected_text, *self.EMPTY_CART_TEXT)
