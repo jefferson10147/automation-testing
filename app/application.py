e83587d8cd32812230fe6505b74550e127ca0d35
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-06 17:48:46 -0400

    Connect the app with main page object model

--- app/application.py
+++ app/application.py
@@ -1,5 +1,6 @@
 from pages.sign_in_page import SignIn
 from pages.help_page import HelpPage
+from pages.main_page import MainPage
 
 
 class Application:
@@ -8,3 +9,4 @@ class Application:
         self.driver = driver
         self.sign_in_page = SignIn(self.driver)
         self.help_page = HelpPage(self.driver)
+        self.main_page = MainPage(self.driver)
