e347dd45e5efc6cb29ff44aeda06797a158265d4
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:03:03 -0400

    Define the steps to verify there are 5 links into best sellers page

---
+++ features/steps/best_sellers_steps.py
@@ -0,0 +1,12 @@
+from behave import given, then
+
+
+@given('Open Amazon best sellers page')
+def open_page(context):
+    context.app.best_sellers.open_best_sellers_page()
+
+
+@then('Verify there are {number_of_links} links')
+def verify_links(context, number_of_links):
+    context.app.best_sellers.locate_elements()
+    context.app.best_sellers.verify_links(int(number_of_links))
