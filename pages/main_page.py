9e7f71571af917507ff87f3105caa5f07e8004c0
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:13:06 -0400

    Delete some unnecessary imports

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,6 @@
-from msilib.schema import Class
 from pages.base_page import Page
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 class MainPage(Page):
