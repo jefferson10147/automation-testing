e2062c95035af02e7c7cee20172a840241547cbd
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:10:45 -0400

    Formate the file

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,14 @@ from msilib.schema import Class
 from pages.base_page import Page
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class MainPage(Page):
 
     CART = (By.ID, 'nav-cart-count-container')
     EMPTY_CART_TEXT = (By.CSS_SELECTOR, '.a-row.sc-your-amazon-cart-is-empty h2')
+    SEARCH_BAR = (By.ID, 'twotabsearchtextbox')
 
     def open_amazon_main_page(self):
         self.open_page()
@@ -18,3 +20,7 @@ class MainPage(Page):
     def verify_empty_cart_text(self):
         expected_text = 'Your Amazon Cart is empty'
         self.verify_text(expected_text, *self.EMPTY_CART_TEXT)
+
+    def input_into_search_bar(self, text: str):
+        self.input_text(text, *self.SEARCH_BAR)
+        self.input_keys(Keys.BACK_SPACE, *self.SEARCH_BAR)
