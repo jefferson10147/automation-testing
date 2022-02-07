635283243d37daa729f896298fc639611eedc131
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:22:33 -0400

    Entry function to verify the number  of items in the cart

--- pages/nav_bar.py
+++ pages/nav_bar.py
@@ -7,7 +7,11 @@ from selenium.webdriver.common.keys import Keys
 class NavBar(Page):
 
     SEARCH_BAR = (By.ID, 'twotabsearchtextbox')
+    CART_COUNT = (By.ID, 'nav-cart-count')
 
     def input_into_search_bar(self, text: str):
         self.input_text(text, *self.SEARCH_BAR)
         self.input_keys(Keys.BACK_SPACE, *self.SEARCH_BAR)
+
+    def verify_cart_count(self, number_of_items='1'):
+        self.verify_text(number_of_items, *self.CART_COUNT)
