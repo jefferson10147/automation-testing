3225c4a817336db76f6a10da3d97a967d77549ac
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 06:57:10 -0400

    Connect best sellers page object with app

--- app/application.py
+++ app/application.py
@@ -1,6 +1,7 @@
 from pages.sign_in_page import SignIn
 from pages.help_page import HelpPage
 from pages.main_page import MainPage
+from pages.best_sellers import BestSellers
 
 
 class Application:
@@ -10,3 +11,4 @@ class Application:
         self.sign_in_page = SignIn(self.driver)
         self.help_page = HelpPage(self.driver)
         self.main_page = MainPage(self.driver)
+        self.best_sellers = BestSellers(self.driver)
