d26742a9d568eedb9e43f6780ee7716bea2863a5
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:30:19 -0400

    Connect nav bar and product page object model to ap

--- app/application.py
+++ app/application.py
@@ -2,6 +2,8 @@ from pages.sign_in_page import SignIn
 from pages.help_page import HelpPage
 from pages.main_page import MainPage
 from pages.best_sellers import BestSellers
+from pages.product_page import ProductPage
+from pages.nav_bar import NavBar
 
 
 class Application:
@@ -12,3 +14,5 @@ class Application:
         self.help_page = HelpPage(self.driver)
         self.main_page = MainPage(self.driver)
         self.best_sellers = BestSellers(self.driver)
+        self.product_page = ProductPage(self.driver)
+        self.nav_bar = NavBar(self.driver)
