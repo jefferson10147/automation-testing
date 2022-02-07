be3b8b371f79d2f8e8dc954dddd71394bbf5ae4b
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 07:12:11 -0400

    Delete search bar function

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,7 +9,6 @@ class MainPage(Page):
 
     CART = (By.ID, 'nav-cart-count-container')
     EMPTY_CART_TEXT = (By.CSS_SELECTOR, '.a-row.sc-your-amazon-cart-is-empty h2')
-    SEARCH_BAR = (By.ID, 'twotabsearchtextbox')
 
     def open_amazon_main_page(self):
         self.open_page()
@@ -20,7 +19,3 @@ class MainPage(Page):
     def verify_empty_cart_text(self):
         expected_text = 'Your Amazon Cart is empty'
         self.verify_text(expected_text, *self.EMPTY_CART_TEXT)
-
-    def input_into_search_bar(self, text: str):
-        self.input_text(text, *self.SEARCH_BAR)
-        self.input_keys(Keys.BACK_SPACE, *self.SEARCH_BAR)
