413305db03e34699244014c3139718dde8f6d2fc
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-07 06:54:41 -0400

    Best sellers page: locator and entry logic to page object

--- pages/best_sellers.py
+++ pages/best_sellers.py
@@ -1,7 +1,21 @@
 from pages.base_page import Page
 
+from decouple import config
 from selenium.webdriver.common.by import By
 
 
+BEST_SELLERS_PAGE = config('BEST_SELLERS')
+
 class BestSellers(Page):
-    pass
+    
+    BEST_SELLERS_LINKS = (By.CSS_SELECTOR, '[href *= "/ref=zg_bs_tab"]')
+
+    def open_best_sellers_page(self):
+        self.open_page(end_url=BEST_SELLERS_PAGE)
+
+    def locate_elements(self):
+        self.elements = self.find_elements(*self.BEST_SELLERS_LINKS)
+
+    def verify_links(self, numbers_of_links=5):
+        current_number_of_links = len(self.elements)
+        assert numbers_of_links == current_number_of_links, f'Expected {numbers_of_links} but got {current_number_of_links}'
