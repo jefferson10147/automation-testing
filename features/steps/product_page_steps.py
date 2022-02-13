024e749a6eb645d2300042d75542e8bb3a8a36a4
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-13 12:14:59 -0400

    Define the steps for test case scenario: user can see the word regular in every product inside wholefoods page

--- features/steps/product_page_steps.py
+++ features/steps/product_page_steps.py
@@ -6,6 +6,11 @@ def open_page(context, product_id):
     context.app.product_page.open_product_page(product_id)
 
 
+@given('Open Wholefoods page')
+def open_wholefood_page(context):
+    context.app.product_page.open_wholefoods_page()
+
+
 @when('Search in the search field for {product}')
 def search_product(context, product):
     context.app.nav_bar.input_into_search_bar(text=product)
@@ -21,6 +26,11 @@ def add_product(context):
     context.app.product_page.add_to_cart()
 
 
+@when('Click in close button')
+def click_btn(context):
+    context.app.product_page.click_close_btn()
+
+
 @then('Verify there are {number_of_items} products added in the cart')
 def verify_number(context, number_of_items):
     context.app.nav_bar.verify_cart_count(number_of_items)
@@ -29,3 +39,8 @@ def verify_number(context, number_of_items):
 @then('User can click over all colors')
 def verify_colors(context):
     context.app.product_page.verify_colors()
+
+
+@then('Verify the regular word')
+def verify_word(context):
+    context.app.product_page.verfiy_regular_text()
