73269cf48cd58f26d48ff8bf1a73841a6d597e98
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-15 08:22:35 -0400

    Define the step to verify use can click in best sellers links

--- features/steps/best_sellers_steps.py
+++ features/steps/best_sellers_steps.py
@@ -10,3 +10,8 @@ def open_page(context):
 def verify_links(context, number_of_links):
     context.app.best_sellers.locate_elements()
     context.app.best_sellers.verify_links(int(number_of_links))
+
+
+@then('Verify links are clickable')
+def click_links(context):
+    context.app.best_sellers.click_in_links()
