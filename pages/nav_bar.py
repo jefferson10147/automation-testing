0271b0b736e0e814e9b4f4eab13143f015574f7a
Jefferson <jeffersonmontilla.98@gmail.com>
2022-02-16 08:52:26 -0400

    Define functions to select a new deparment

--- pages/nav_bar.py
+++ pages/nav_bar.py
@@ -8,6 +8,9 @@ class NavBar(Page):
 
     SEARCH_BAR = (By.ID, 'twotabsearchtextbox')
     CART_COUNT = (By.ID, 'nav-cart-count')
+    DEPARMENTS_DROPDOWN = (By.ID, 'searchDropdownBox')
+    VIDEOGAMES_OPTION = (
+        By.CSS_SELECTOR, '#searchDropdownBox option[value="search-alias=videogames-intl-ship"]')
 
     def input_into_search_bar(self, text: str):
         self.input_text(text, *self.SEARCH_BAR)
@@ -15,3 +18,9 @@ class NavBar(Page):
 
     def verify_cart_count(self, number_of_items='1'):
         self.verify_text(number_of_items, *self.CART_COUNT)
+
+    def click_in_dropdown(self):
+        self.click(*self.DEPARMENTS_DROPDOWN)
+
+    def click_in_videogames_option(self):
+        self.wait_for_element_click(*self.VIDEOGAMES_OPTION)
